63be3b5823c1857ec1022ec9efc349242a3c67aa
Rajaruban Rajindram <85555894+rubanero14@users.noreply.github.com>
2022-07-18 11:38:24 +0800

    update progress
    update progress

--- app.js
+++ app.js
@@ -3,7 +3,20 @@ const http = require('http');
 
 
 const server = http.createServer((req,res) => {
-    console.log(req);
+    console.log(req.url, req.method, req.headers);
+    // process.exit();
+    res.setHeader('Content-Type', 'text/html');
+    res.write(`
+        <html>
+            <head>
+                <title>My SSR Page</title>
+                <body>
+                    <h1>Hello Node!</h1>
+                </body>
+            </head>
+        </html>
+    `);
+    res.end('Hi');
 });
 
 server.listen(3000);
