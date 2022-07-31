a5d2c2df366e49448fc71eeca22458f028ad1f98
Rajaruban Rajindram <85555894+rubanero14@users.noreply.github.com>
2022-07-31 21:06:11 +0800

    update progress
    update progress

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
 const http = require('http');
+
+const express = require('express');
 const PORT = 3000;
 
-// importing the custom module created in routes.js
-const routes = require('./routes'); // .js is automatically appended by Node into filepath
+const app = express();
 
+// importing the custom module created in routes.js
+// const routes = require('./routes'); // .js is automatically appended by Node into filepath
 
 // Now just pass the imported route function into createServer(routes) as an arguement and omit the parentheses if single function
 // is exported
 // for more than one item exported, we can access the function using key/value pairing as below
-console.log(routes.someText);
-const server = http.createServer(routes.handler);
+// console.log(routes.someText);
+const server = http.createServer(app);
 
 // Listen to server
 server.listen(PORT);
