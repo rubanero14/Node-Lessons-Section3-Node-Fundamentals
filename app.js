23705f5d1f16b8e2e930217e224e77e5ddc58b64
Rajaruban Rajindram <85555894+rubanero14@users.noreply.github.com>
2022-08-17 20:54:13 +0800

    EJS Partial Layout
    EJS Partial Layout - Splitting EJS files into different component and conditionally import and render into template files

--- app.js
+++ app.js
@@ -68,6 +68,7 @@ app.use((req,res,next) => {
     // res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
     res.status(404).render('404', {
         docTitle: '404: Page Not Found',
+        path: undefined,
     });
 });
 
